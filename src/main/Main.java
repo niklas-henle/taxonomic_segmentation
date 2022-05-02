7b20332d9d8b6fa9a5d62234d972503d619d4040
niklas-henle <39056541+niklas-henle@users.noreply.github.com>
2022-05-02 11:56:09 +0200

    Create connection to database and a simple url connection to ncbi gathering the information about the taxonomic id.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,13 @@ import utils.*;
 import models.*;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
+   /**
     public static void main(String[] args) throws IOException {
 
         HashMap<String, TSSeq> tSSeqHashMap = Utils.fastAParser(args[0]);
@@ -16,6 +18,7 @@ public class Main {
         tSSeqHashMap = Utils.generateSegments(blastTabFile, tSSeqHashMap);
 
         int maxLength = 0;
+
         for (TSSeq test: tSSeqHashMap.values()) {
             for (ArrayList<String> seg : test.getSegmentation().getSegmentation()
             ) {
@@ -27,4 +30,18 @@ public class Main {
         }
         System.out.println(maxLength);
     }
+    **/
+
+   public static void main(String[] args) {
+       try {
+           DatabaseConnector dbConnector = new DatabaseConnector(args[2]);
+           dbConnector.queryAccessionNumber("WP_042695806.1");
+           String taxa = Utils.getTaxonomyFromTid(dbConnector.queryAccessionNumber("WP_042695806.1"));
+           System.out.println(taxa);
+           dbConnector.closeConnection();
+       } catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
+
+   }
 }
---
+++ src/utils/DatabaseConnector.java
@@ -0,0 +1,61 @@
+package utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Database connection class containing helper functions to query and close connection
+ */
+public class DatabaseConnector {
+
+    Connection connection;
+
+    /**
+     * Constructor for the DatabaseConnector. Connect to the local database provided in db.
+     * @param db path to the database
+     * @throws SQLException SQL error
+     */
+    public DatabaseConnector(String db) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:"+db);
+    }
+
+
+    /**
+     * Use the Accession number to retrieve the taxonomy id.
+     * @param an Accession number
+     * @return taxonomy id
+     */
+    public String queryAccessionNumber(String an) {
+        an = an.split("\\.")[0]; // Strip accession number of version
+
+        try {
+            Statement query = connection.createStatement();
+            ResultSet res = query.executeQuery(String.format("SELECT TAXONOMY FROM mappings WHERE Accession='%s'", an));
+            return res.getString("Taxonomy");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    /**
+     * Close the current connection
+     */
+    public void closeConnection() {
+        try
+        {
+            if(connection != null)
+                connection.close();
+        }
+        catch(SQLException e)
+        {
+            // connection close failed.
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
